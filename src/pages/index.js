98f7f006604f049a80fcf16c07339be0eb8b757b
Hector Mendoza <hectormauriciomx@gmail.com>
2023-09-11 10:51:35 -0600

    daily commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
+import { useRouter } from "next/router";
+import { Button } from "@mui/material";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ name, summary }) {
+  const router = useRouter();
+
   return (
     <section className={styles.Home}>
       <h1 className={styles.Name}>{name}</h1>
       <div className={styles.summary}>{summary}</div>
+      <div>
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => router.push("/projects")}
+        ></Button>
+      </div>
     </section>
   );
 }
