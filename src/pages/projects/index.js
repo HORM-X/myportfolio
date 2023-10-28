a08ddb4bada0364455d6eb5828a03b7672f62bd3
Hector Mendoza <hectormauriciomx@gmail.com>
2023-10-27 21:55:04 -0600

    Duplicated file deleted. Page description added.

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,3 +1,28 @@
+import PageDescription from "@/components/PageDescription";
+
 export default function ProjectsPage() {
-  return <h1>Projects</h1>;
+  return (
+    <section>
+      <PageDescription
+        title="Projects"
+        description="Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology"
+      />
+
+      {/*      {projects.map((project) => (
+        <ProjectItem key={project._id} />
+      ))}*/}
+    </section>
+  );
+}
+
+{
+  /*
+export async function getServerSideProps() {
+  return {
+    props: {
+      projects: [],
+    },
+  };
+}
+*/
 }
