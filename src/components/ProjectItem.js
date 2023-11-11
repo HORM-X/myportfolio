97b5c12ce27020330b9795f0610d3afc9c704b1c
Hector Mendoza <hectormauriciomx@gmail.com>
2023-11-11 00:02:42 -0600

    Bug fixed. Projects finally shown on page.

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,7 +1,9 @@
 import { useRouter } from "next/router";
 import { Grid, Button, Stack } from "@mui/material";
 
-export default function ProjectItem({ project, handleDelete, handleEdit }) {
+export default function ProjectItem({ project }) {
+  const router = useRouter();
+
   return (
     <Grid container spacing={6} sx={{ pb: "40px" }}>
       <Grid item md={6}>
@@ -15,11 +17,13 @@ export default function ProjectItem({ project, handleDelete, handleEdit }) {
         <Stack spacing={4}>
           <h3>{project.name}</h3>
           <div>{project.description}</div>
-          <ItemActions
-            id={project._id}
-            onDelete={handleDelete}
-            onEdit={handleEdit}
-          />
+          <Button
+            variant="contained"
+            size="large"
+            onClick={() => router.push(`/projects/${project._id}`)}
+          >
+            Use Case
+          </Button>
         </Stack>
       </Grid>
     </Grid>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,6 +1,7 @@
 import PageDescription from "@/components/PageDescription";
+import ProjectItem from "@/components/ProjectItem";
 
-export default function ProjectsPage() {
+export default function ProjectsPage({ projects }) {
   return (
     <section>
       <PageDescription
@@ -8,21 +9,47 @@ export default function ProjectsPage() {
         description="Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology"
       />
 
-      {/*      {projects.map((project) => (
-        <ProjectItem key={project._id} />
-      ))}*/}
+      {projects.map((project) => (
+        <ProjectItem key={project._id} project={project} />
+      ))}
     </section>
   );
 }
 
-{
-  /*
 export async function getServerSideProps() {
+  let projects = [
+    {
+      _id: 1,
+      name: "Dopefolio",
+      description:
+        "Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally",
+      imageUrl:
+        "https://d33wubrfki0l68.cloudfront.net/19c708670a1f21231c1e2efa6ba38dbf52b15343/3237e/assets/jpeg/dopefolio.jpeg",
+      tools: ["JS", "PHP"],
+    },
+    {
+      _id: 2,
+      name: "Wilsonport",
+      description:
+        "Wilsonport is a multiservice logistics and transport company and I created their website from scratch using the frontend tools I know.",
+      imageUrl:
+        "https://d33wubrfki0l68.cloudfront.net/9199afe42f789dbddb324ed3edd326e080e693c1/28f54/assets/jpeg/wilsonport.jpeg",
+      tools: ["Typescript", "Next"],
+    },
+  ];
+
+  /*{
+  try {
+    const response = await fetch("http://localhost:3000/api/projects");
+    projects = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}*/
+
   return {
     props: {
-      projects: [],
+      projects,
     },
   };
-}
-*/
 }
